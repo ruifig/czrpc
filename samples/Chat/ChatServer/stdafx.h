09485f035ee202163e897a6712e8ffd412066c25
Rui Figueira <ruimvfigueira@gmail.com>
2016-05-31 00:35:45 +0100

    initial work on Chat sample

---
+++ samples/Chat/ChatServer/stdafx.h
@@ -0,0 +1,16 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <string>
+
+#include "crazygaze/rpc/RPC.h"
+#include "crazygaze/rpc/RPCAsioTransport.h"
+#include "../ChatCommon/ChatInterface.h"
