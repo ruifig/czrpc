94af85727676474457f5a925a50cba7828174185
Rui Figueira <ruimvfigueira@gmail.com>
2016-11-15 00:17:34 +0000

    imported patch linuxfixes4

--- samples/SamplesCommon/Misc.cpp
+++ samples/SamplesCommon/Misc.cpp
@@ -2,191 +2,68 @@
 #include "Misc.h"
 
 #ifdef __linux
-#include <termios.h>
 #include <sys/select.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/ioctl.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <stropts.h>
-
-static struct termios old, new_;
-
-/* Initialize new terminal i/o settings */
-static void initTermios(int echo)
-{
-	tcgetattr(0, &old); /* grab old terminal i/o settings */
-	new_ = old; /* make new settings same as old settings */
-	new_.c_lflag &= ~ICANON; /* disable buffered i/o */
-	new_.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
-	tcsetattr(0, TCSANOW, &new_); /* use these new terminal i/o settings now */
-}
-
-/* Restore old terminal i/o settings */
-static void resetTermios(void)
-{
-	tcsetattr(0, TCSANOW, &old);
-}
-
-/* Read 1 character - echo defines echo mode */
-static char getch_(int echo)
-{
-	char ch;
-	initTermios(echo);
-	ch = getchar();
-	resetTermios();
-	return ch;
-}
-
-/* Read 1 character without echo */
-char my_getch(void)
-{
-	return getch_(0);
-}
-
-void changemode(int dir)
-{
-  static struct termios oldt, newt;
-
-  if ( dir == 1 )
-  {
-    tcgetattr( STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~( ICANON | ECHO );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newt);
-  }
-  else
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
-}
-
-#if 0
-int my_kbhit(void)
-{
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
-
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-  ch = getchar();
-
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-  if(ch != EOF)
-  {
-    //ungetc(ch, stdin);
-    return 1;
-  }
-
-  return 0;
-}
+#elif _WIN32
+#include <conio.h>
 #endif
 
-static const int STDIN = 0;
-int my_kbhit() {
-    static bool initialized = false;
+#ifdef __linux__
 
-    if (! initialized) {
-        // Use termios to turn off line buffering
-        termios term;
-        tcgetattr(STDIN, &term);
-        term.c_lflag &= ~(ICANON);
-        tcsetattr(STDIN, TCSANOW, &term);
-        setbuf(stdin, NULL);
-        initialized = true;
-    }
-
-    int bytesWaiting;
-    ioctl(STDIN, FIONREAD, &bytesWaiting);
-	 if (bytesWaiting)
-		 my_getch();
-    return bytesWaiting;
-}
-
-bool try_getline(std::string& str)
+static termios term_save()
 {
-	termios oldt, newt;
-	tcgetattr(STDIN, &oldt);
-
-	// Disable echo
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN, TCSANOW, &newt);
-
-	// check if there are any keys pending
-	int bytesWaiting;
-	ioctl(STDIN, FIONREAD, &bytesWaiting);
-
-	if (bytesWaiting)
-	{
-		str = "";
-		while(bytesWaiting--)
-			str += getchar();
-
-		// enable echo
-		oldt.c_lflag &= ~ECHO;
-		tcsetattr(STDIN, TCSANOW, &oldt);
-		//newt.c_lflag |= ECHO;
-		//tcsetattr(STDIN, TCSANOW, &newt);
-
-		std::cout << "COMMAND> " << str;
-		std::string tmp;
-		std::getline(std::cin, tmp);
-		str += tmp;
-		printf("*%s\n", str.c_str());
-	}
-
-	oldt.c_lflag &= ~ECHO;
-	tcsetattr(STDIN, TCSANOW, &oldt);
-	return str!="";
+    termios t;
+    tcgetattr(STDIN_FILENO, &t);
+    return t;
 }
 
-#if 0
-int my_kbhit (void)
+static void term_restore(termios& t)
 {
-	//changemode(1);
-  struct timeval tv;
-  fd_set rdfs;
-
-  tv.tv_sec = 0;
-  tv.tv_usec = 0;
-
-  FD_ZERO(&rdfs);
-  FD_SET (STDIN_FILENO, &rdfs);
-
-  select(STDIN_FILENO+1, &rdfs, NULL, NULL, &tv);
-	//changemode(0);
-  return FD_ISSET(STDIN_FILENO, &rdfs);
-
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
 }
-#endif
 
-#if 0
-// From http://www.flipcode.com/archives/_kbhit_for_Linux.shtml
-int my_kbhit() {
-	timeval timeout;
-	fd_set rdset;
-
-	FD_ZERO(&rdset);
-	FD_SET(STDIN, &rdset);
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 0;
-
-	return select(STDIN + 1, &rdset, NULL, NULL, &timeout);
+static void term_setEcho(bool val)
+{
+    termios t;
+    tcgetattr(STDIN_FILENO, &t);
+    if (val)
+        t.c_lflag |= ECHO;
+    else
+        t.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
 }
-#endif
 
-#else
+static void term_setBuffering(bool val)
+{
+    termios t;
+    tcgetattr(STDIN_FILENO, &t);
+    if (val)
+        t.c_lflag |= ICANON;
+    else
+        t.c_lflag &= ~ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+}
 
-char my_getch()
+int my_getch()
+{
+	return getchar();
+}
+
+int my_kbhit()
+{
+	struct timeval tv;
+	fd_set fds;
+	tv.tv_sec = 0;
+	tv.tv_usec = 0;
+	FD_ZERO(&fds);
+	FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+	select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
+	return FD_ISSET(STDIN_FILENO, &fds);
+}
+
+#elif _WIN32
+
+int my_getch()
 {
 	return _getch();
 }
@@ -199,3 +76,49 @@ int my_kbhit()
 #endif
 
 
+
+CommandLineReader::CommandLineReader(std::string prompt)
+            : m_prompt(prompt)
+    {
+#ifdef __linux__
+	m_originalt = term_save();
+	term_setBuffering(false);
+	term_setEcho(false);
+#elif _WIN32
+#endif
+	std::cout << "Press ENTER to go into command mode." << std::endl;
+}
+
+CommandLineReader::~CommandLineReader()
+{
+#ifdef __linux__
+	term_restore(m_originalt);
+#endif
+}
+
+bool CommandLineReader::tryGet(std::string& cmd)
+{
+	bool ok = false;
+	while (my_kbhit())
+	{
+		int c = my_getch();
+		if (c == '\n' || c == '\r')
+			ok = true;
+	}
+
+	if (!ok)
+		return false;
+
+#ifdef __linux__
+	term_setBuffering(true);
+	term_setEcho(true);
+#endif
+	std::cout << m_prompt;
+	cmd = "";
+	std::getline(std::cin, cmd);
+#ifdef __linux__
+	term_setBuffering(false);
+	term_setEcho(false);
+#endif
+	return true;
+}
--- samples/SamplesCommon/Misc.h
+++ samples/SamplesCommon/Misc.h
@@ -1,12 +1,31 @@
 #pragma once
 
+#include <string>
+
+#ifdef __linux__
+#include <termio.h>
+#endif
+
 // Cross platform getch()
 // Note:
 // It will return as soon as a key is pressed.
 // Seems to work fine on a linux terminal (doesn't require to hit Enter), but if running inside Clion, it seems we
 // still need to press Enter
-char my_getch();
+int my_getch();
+
 
 int my_kbhit();
-bool try_getline(std::string& str);
 
+
+class CommandLineReader
+{
+public:
+	explicit CommandLineReader(std::string prompt = "");
+	~CommandLineReader();
+    bool tryGet(std::string& cmd);
+private:
+    std::string m_prompt;
+#ifdef __linux__
+    termios m_originalt;
+#endif
+};
