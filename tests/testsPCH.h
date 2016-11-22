9e469fdb5bf81a06dc6fc717d30863575747844d
Rui Figueira <ruimvfigueira@gmail.com>
2016-11-22 00:20:10 +0000

    cleanup

--- tests/testsPCH.h
+++ tests/testsPCH.h
@@ -4,7 +4,11 @@
 #include "targetver.h"
 #endif
 
-#define SHORT_TESTS 0
+
+#define SHORT_TESTS 1
+
+// If set to 1, and running on Debug and Windows, it will enable some more CRT memory debug things
+#define ENABLE_MEM_DEBUG 0
 
 //
 // czrpc
