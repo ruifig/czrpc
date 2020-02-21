cac0b5cf99871e490949350cd926165687bc6998
ruifig <ruimvfigueira@gmail.com>
2020-02-21 22:08:18 +0000

    removed UnitTest++ and cleanup

--- gen_vs2017.bat
+++
@@ -1,6 +0,0 @@
-rmdir /q /s build
-md build64
-cd build64
-cmake -G "Visual Studio 15 Win64" -T "v141" ..
-cd ..
-
--- gen_vs2019.bat
+++
@@ -1,6 +0,0 @@
-rmdir /q /s build
-md build64
-cd build64
-cmake -G "Visual Studio 16 2019" -A x64 ..
-cd ..
-
